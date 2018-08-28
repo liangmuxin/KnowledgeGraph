15a2e5f972420c2bec18f1437afd01cb138e6a88
liangmuxin <1441793911@qq.com>
2018-08-27 22:58:43 -0700

    8/27

--- hw1/hw1_scrapy/hw1_scrapy/spiders/myspiders.py
+++ hw1/hw1_scrapy/hw1_scrapy/spiders/myspiders.py
@@ -1,4 +1,6 @@
 import scrapy
+import datetime
+import uuid
 
 
 def get_urls(filename):
@@ -9,16 +11,49 @@ def get_urls(filename):
     return res
 
 
+TOTAL_ARTISTS = 5000
+TOTAL_ARTWORKS = 3000
+
+
 class ArtistSpider(scrapy.Spider):
     name = "artist"
 
     def start_requests(self):
         start_urls = get_urls("test_artist.txt")
-        print(start_urls)
-        print("*******************************************")
-        urls = ['http://quotes.toscrape.com/page/1/', 'http://quotes.toscrape.com/page/2/']
+        # print(start_urls)
+        # print("*******************************************")
+        seedurl = ['https://americanart.si.edu/art/artists']
         for url in start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse1)
+    # scrapy.Request(url) can return the GET
 
-    def parse(self, response):
-        yield response.url
+    def parse1(self, response):
+        out = {}
+        out["raw_content"] = str(response.body)
+        out["url"] = str(response.url)
+        out["timestamp_crawl"] = int(datetime.datetime.now().timestamp())
+        out["doc_id"] = str(uuid.uuid4())
+
+        return
+
+
+class ArtworkSpider(scrapy.Spider):
+    name = "artwork"
+
+    def start_requests(self):
+        start_urls = get_urls("test_artist.txt")
+        # print(start_urls)
+        # print("*******************************************")
+        seedurl = ['https://americanart.si.edu/art/artists']
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse1)
+    # scrapy.Request(url) can return the GET
+
+    def parse1(self, response):
+        out = {}
+        out["raw_content"] = str(response.body)
+        out["url"] = str(response.url)
+        out["timestamp_crawl"] = int(datetime.datetime.now().timestamp())
+        out["doc_id"] = str(uuid.uuid4())
+
+        return
