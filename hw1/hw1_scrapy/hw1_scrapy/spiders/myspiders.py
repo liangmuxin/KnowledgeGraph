d8b42093af8908f3655c203f820c9ef4375e9461
liangmuxin <muxin@MuxindeMacBook-Pro.local>
2018-08-28 21:37:31 -0700

    latest version

--- hw1/hw1_scrapy/hw1_scrapy/spiders/myspiders.py
+++ hw1/hw1_scrapy/hw1_scrapy/spiders/myspiders.py
@@ -1,6 +1,7 @@
 import scrapy
 import datetime
 import uuid
+import os
 
 
 def get_urls(filename):
@@ -17,15 +18,17 @@ TOTAL_ARTWORKS = 3000
 
 class ArtistSpider(scrapy.Spider):
     name = "artist"
+    user_agent = 'Mozilla/5.0'
 
     def start_requests(self):
-        start_urls = get_urls("test_artist.txt")
-        # print(start_urls)
-        # print("*******************************************")
+        start_urls = get_urls("mandatory_artists.txt")
         seedurl = ['https://americanart.si.edu/art/artists']
         for url in start_urls:
+            # self.log("collecting %s" % url)
             yield scrapy.Request(url=url, callback=self.parse1)
     # scrapy.Request(url) can return the GET
+        for url in seedurl:
+            yield scrapy.Request(url=url, callback=self.parse2)
 
     def parse1(self, response):
         out = {}
@@ -33,20 +36,49 @@ class ArtistSpider(scrapy.Spider):
         out["url"] = str(response.url)
         out["timestamp_crawl"] = int(datetime.datetime.now().timestamp())
         out["doc_id"] = str(uuid.uuid4())
+        yield out
 
-        return
+    def parse2(self, response):
+        path = "//tr/td[1]/a/@href"
+        for url in response.xpath(path).extract():   
+            yield scrapy.Request(url="https://americanart.si.edu"+url, callback=self.parse1)  
+        page = response.css("li.pager__item.pager__item--next a::attr(href)").extract()
+        if page:
+            yield scrapy.Request(url="https://americanart.si.edu/art/artists"+page[0], callback=self.parse2)
+
+        # for url in response.xpath(path).extract():
+        #     import pdb
+        #     pdb.set_trace()
+        #     self.log("start crawling %s" % url)
+
+        #     scrapy.Request(url="https://americanart.si.edu"+url, callback=self.parse2)
+
+    # def parse3(self, response):
+    #     scrapy.Request(url=response.url, callback=self.parse4)
+    #     page = response.css("li.pager__item.pager__item--next a::attr(href)").extract()
+    #     if page:
+    #         scrapy.Request(url=response.url+page[0], callback=self.parse3)
+
+    # def parse4(self, response):
+    #     path = "//td/a/@href"
+    #     for url in response.xpath(path).extract():
+    #         scrapy.Request(url="https://americanart.si.edu"+url, callback=self.parse1)
 
 
 class ArtworkSpider(scrapy.Spider):
     name = "artwork"
+    user_agent = 'Mozilla/5.0'
+    val_count = 0
 
     def start_requests(self):
-        start_urls = get_urls("test_artist.txt")
-        # print(start_urls)
-        # print("*******************************************")
-        seedurl = ['https://americanart.si.edu/art/artists']
+        start_urls = get_urls("mandatory_artworks.txt")
+        seedurl = ['https://americanart.si.edu/art/browse?classification=All&mediums=All&location=All&on_view=0']
         for url in start_urls:
             yield scrapy.Request(url=url, callback=self.parse1)
+
+
+        for url in seedurl:
+            yield scrapy.Request(url=url, callback=self.parse2)
     # scrapy.Request(url) can return the GET
 
     def parse1(self, response):
@@ -55,5 +87,29 @@ class ArtworkSpider(scrapy.Spider):
         out["url"] = str(response.url)
         out["timestamp_crawl"] = int(datetime.datetime.now().timestamp())
         out["doc_id"] = str(uuid.uuid4())
+        self.val_count += 1
+        yield out
 
-        return
+    def parse2(self, response):
+        # print(response.url)
+        # print("反弹"*10)
+        # tmp = response.url
+        # yield scrapy.Request(url=tmp, callback=self.parse3)
+        # print("LGD is champion!") 
+        path = "//div/h4/a/@href"
+        for url in response.xpath(path).extract():   
+            yield scrapy.Request(url="https://americanart.si.edu"+url, callback=self.parse1)     
+        page = response.css("li.next a::attr(href)").extract()
+        # import pdb
+        # pdb.set_trace()
+        if page:
+            yield scrapy.Request(url='https://americanart.si.edu/art/browse?classification=All&mediums=All&location=All&on_view=0'+"&"+page[0].split("&")[-1], callback=self.parse2)
+
+    # def parse3(self, response):
+    #     import pdb
+    #     pdb.set_trace()
+    #     print(response.url) 
+    #     print("shabi"*10)   
+    #     path = "//div/h4/a/@href"
+    #     for url in response.xpath(path).extract():
+    #         yield scrapy.Request(url="https://americanart.si.edu"+url, callback=self.parse1)
